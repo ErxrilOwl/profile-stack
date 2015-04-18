59222f1ef187bfec067fe1e95e0a71023e417bb0
bensoer <ikben@shaw.ca>
2015-04-17 21:09:24 -0700

    added bunch of contact page and some home page

--- app/app.js
+++ app/app.js
@@ -5,8 +5,10 @@ angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
+  'myApp.home',
+  'myApp.contact',
   'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/home'});
 }]);
