4349f1328ea24cfc8e0a5a0aaed142e6db2143e1
bensoer <ben@soernet.ca>
2016-12-29 13:21:49 -0800

    added error handling for about page

--- angularjs-client/app/about/aboutController.js
+++ angularjs-client/app/about/aboutController.js
@@ -12,20 +12,25 @@ angular.module('myApp.about', ['ngRoute'])
     .controller('aboutController', ['$scope', '$rootScope', '$log', '$http', 'BASEURL', 'USERID', '$sce',
         function($scope, $rootScope, $log, $http, BASEURL, USERID, $sce) {
 
+            $log.info("About - Initializing");
+
             //load the about information
             $scope.showAboutLoader = true;
+            $scope.showErrorMessage = false;
 
             $scope.aboutData = [];
 
             $http.get(BASEURL + "/api/user/" + USERID + "/about").then(function(response){
-                $log.info("SUCCESS");
-                $log.info(response);
                 $scope.aboutData = response.data;
                 $scope.showAboutLoader = false;
+                $scope.showErrorMessage = false;
+                $rootScope.CondensedMode = false;
             },function(response){
                 $log.error("About - There Was An Error Fetching The About Information");
                 $log.error(response);
                 $scope.showAboutLoader = false;
+                $scope.showErrorMessage = true;
+                $rootScope.CondensedMode = true;
             });
 
             $scope.renderHTML = function(htmlCode){
